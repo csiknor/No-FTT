7353e3d35bec9a0141f2ae021f32450fd73b8b67
Norbert Csik <norbert.csik@icloud.com>
2024-12-15 17:19:59 +0100

    feat: gets profile

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,12 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, input)
+import Html exposing (Html, div, input, text)
 import Html.Attributes exposing (placeholder, value)
 import Html.Events exposing (onInput)
+import Http exposing (Expect, emptyBody, header)
+import Json.Decode exposing (Decoder, field, int, list, map3, string)
+import Platform.Cmd as Cmd
 
 
 
@@ -12,11 +15,18 @@ import Html.Events exposing (onInput)
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element { init = init, update = update, subscriptions = subscriptions, view = view }
 
 
 
 -- MODEL
+
+
+type alias Profile =
+    { id : Int
+    , typ : String
+    , fullName : String
+    }
 
 
 type ApiKey
@@ -26,12 +36,15 @@ type ApiKey
 
 type alias Model =
     { apiKey : ApiKey
+    , profile : Maybe Profile
     }
 
 
-init : Model
-init =
-    { apiKey = Unknown }
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { apiKey = Unknown, profile = Nothing }
+    , Cmd.none
+    )
 
 
 
@@ -40,13 +53,31 @@ init =
 
 type Msg
     = ChangeApiKey String
+    | GotProfiles (Result Http.Error (List Profile))
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ChangeApiKey key ->
-            { model | apiKey = Wise key }
+            ( { model | apiKey = Wise key }, getPersonalProfile key )
+
+        GotProfiles response ->
+            case response of
+                Err _ ->
+                    ( model, Cmd.none )
+
+                Ok profiles ->
+                    ( { model | profile = List.head (List.filter (\p -> p.typ == "PERSONAL") profiles) }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
 
 
 
@@ -57,6 +88,14 @@ view : Model -> Html Msg
 view model =
     div []
         [ input [ placeholder "Enter your API key", value (myApiKey model.apiKey), onInput ChangeApiKey ] []
+        , text
+            (case model.profile of
+                Nothing ->
+                    ""
+
+                Just p ->
+                    "Logged in as " ++ p.fullName
+            )
         ]
 
 
@@ -68,3 +107,53 @@ myApiKey apiKey =
 
         Wise key ->
             key
+
+
+
+-- HTTP
+
+
+wiseUrl : String
+wiseUrl =
+    "https://api.transferwise.com"
+
+
+profilesApi : String
+profilesApi =
+    "/v2/profiles"
+
+
+apiGet :
+    { url : String
+    , expect : Expect msg
+    , token : String
+    }
+    -> Cmd msg
+apiGet req =
+    Http.request
+        { method = "GET"
+        , headers = [ header "Authorization" ("Bearer " ++ req.token) ]
+        , url = req.url
+        , body = emptyBody
+        , expect = req.expect
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+getPersonalProfile : String -> Cmd Msg
+getPersonalProfile token =
+    apiGet { url = wiseUrl ++ profilesApi, expect = Http.expectJson GotProfiles profilesDecoder, token = token }
+
+
+profilesDecoder : Decoder (List Profile)
+profilesDecoder =
+    list profileDecoder
+
+
+profileDecoder : Decoder Profile
+profileDecoder =
+    map3 Profile
+        (field "id" int)
+        (field "type" string)
+        (field "fullName" string)
