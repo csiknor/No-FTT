7a78e36eb92f8acc1f5394c7ba24daf09217eee6
Norbert Csik <norbert.csik@icloud.com>
2024-12-15 19:28:38 +0100

    feat: gets balances

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,13 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, input, text)
+import Html exposing (Html, div, input, li, text, ul)
 import Html.Attributes exposing (placeholder, value)
 import Html.Events exposing (onInput)
 import Http exposing (Expect, emptyBody, header)
-import Json.Decode exposing (Decoder, field, int, list, map3, string)
+import Json.Decode exposing (Decoder, at, field, float, int, list, map3, map4, nullable, string)
 import Platform.Cmd as Cmd
+import Url.Builder as B
 
 
 
@@ -29,6 +30,14 @@ type alias Profile =
     }
 
 
+type alias Balance =
+    { id : Int
+    , currency : String
+    , name : Maybe String
+    , amount : Float
+    }
+
+
 type ApiKey
     = Unknown
     | Wise String
@@ -37,12 +46,13 @@ type ApiKey
 type alias Model =
     { apiKey : ApiKey
     , profile : Maybe Profile
+    , balances : Maybe (List Balance)
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { apiKey = Unknown, profile = Nothing }
+    ( { apiKey = Unknown, profile = Nothing, balances = Nothing }
     , Cmd.none
     )
 
@@ -54,6 +64,7 @@ init _ =
 type Msg
     = ChangeApiKey String
     | GotProfiles (Result Http.Error (List Profile))
+    | GotBalances (Result Http.Error (List Balance))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -68,7 +79,41 @@ update msg model =
                     ( model, Cmd.none )
 
                 Ok profiles ->
-                    ( { model | profile = List.head (List.filter (\p -> p.typ == "PERSONAL") profiles) }, Cmd.none )
+                    let
+                        profile =
+                            findPersonalProfile profiles
+                    in
+                    ( { model | profile = profile }
+                    , case model.apiKey of
+                        Wise key ->
+                            case profile of
+                                Just p ->
+                                    getBalances key p
+
+                                Nothing ->
+                                    Cmd.none
+
+                        Unknown ->
+                            Cmd.none
+                    )
+
+        GotBalances response ->
+            case response of
+                Err _ ->
+                    ( model, Cmd.none )
+
+                Ok balances ->
+                    ( { model | balances = Just balances }, Cmd.none )
+
+
+findPersonalProfile : List Profile -> Maybe Profile
+findPersonalProfile profiles =
+    List.head (List.filter isPersonalProfile profiles)
+
+
+isPersonalProfile : Profile -> Bool
+isPersonalProfile p =
+    p.typ == "PERSONAL"
 
 
 
@@ -88,15 +133,30 @@ view : Model -> Html Msg
 view model =
     div []
         [ input [ placeholder "Enter your API key", value (myApiKey model.apiKey), onInput ChangeApiKey ] []
-        , text
-            (case model.profile of
-                Nothing ->
-                    ""
+        , div []
+            [ text
+                (case model.profile of
+                    Nothing ->
+                        ""
 
-                Just p ->
-                    "Logged in as " ++ p.fullName
-            )
+                    Just p ->
+                        "Logged in as " ++ p.fullName
+                )
+            ]
+        , div []
+            [ case model.balances of
+                Nothing ->
+                    text ""
+
+                Just balances ->
+                    ul [] (List.map balanceView balances)
+            ]
         ]
+
+
+balanceView : Balance -> Html msg
+balanceView balance =
+    li [] [ text (balance.currency ++ " " ++ String.fromFloat balance.amount) ]
 
 
 myApiKey : ApiKey -> String
@@ -143,12 +203,7 @@ apiGet req =
 
 getPersonalProfile : String -> Cmd Msg
 getPersonalProfile token =
-    apiGet { url = wiseUrl ++ profilesApi, expect = Http.expectJson GotProfiles profilesDecoder, token = token }
-
-
-profilesDecoder : Decoder (List Profile)
-profilesDecoder =
-    list profileDecoder
+    apiGet { url = wiseUrl ++ profilesApi, expect = Http.expectJson GotProfiles (list profileDecoder), token = token }
 
 
 profileDecoder : Decoder Profile
@@ -157,3 +212,22 @@ profileDecoder =
         (field "id" int)
         (field "type" string)
         (field "fullName" string)
+
+
+balancesUrl : Int -> String
+balancesUrl id =
+    B.crossOrigin wiseUrl [ "v4", "profiles", String.fromInt id, "balances" ] [ B.string "types" "STANDARD" ]
+
+
+getBalances : String -> Profile -> Cmd Msg
+getBalances token profile =
+    apiGet { url = balancesUrl profile.id, expect = Http.expectJson GotBalances (list balanceDecoder), token = token }
+
+
+balanceDecoder : Decoder Balance
+balanceDecoder =
+    map4 Balance
+        (field "id" int)
+        (field "currency" string)
+        (field "name" (nullable string))
+        (at [ "amount", "value" ] float)
