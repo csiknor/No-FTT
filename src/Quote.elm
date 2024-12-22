8ef1438f159bc8dc2134beede7937990bd55dfeb
Norbert Csik <norbert.csik@icloud.com>
2024-12-22 20:03:43 +0100

    feat: Quote improvements supporting disabled payment options, estimated delivery and notices

--- src/Quote.elm
+++ src/Quote.elm
@@ -4,6 +4,7 @@ module Quote exposing (PaymentOption, Quote, QuoteReq, TransferMethod(..), postQ
 
 import Api exposing (Status(..), wiseApiPost)
 import Html exposing (Html, div, text)
+import Html.Attributes exposing (title)
 import Http
 import Json.Decode as D
 import Json.Encode as E
@@ -33,15 +34,32 @@ type alias Quote =
     , targetAmount : Maybe Float
     , preferredPayIn : String
     , paymentOptions : List PaymentOption
+    , notices : List Notice
     }
 
 
 type alias PaymentOption =
-    { payIn : String
+    { disabled : Bool
+    , estimatedDelivery : String
+    , formattedEstimatedDelivery : String
+    , payIn : String
     , payOut : String
     , feeAmount : Float
     , priceTotalAmount : Float
     }
+
+
+type alias Notice =
+    { text : String
+    , link : String
+    , type_ : NoticeType
+    }
+
+
+type NoticeType
+    = Info
+    | Warning
+    | Blocked
 
 
 
@@ -61,7 +79,8 @@ quoteView status =
                 , div [] [ text ("Target: " ++ quote.targetCurrency ++ " " ++ String.fromFloat (Maybe.withDefault 0 quote.targetAmount)) ]
                 , div [] [ text ("Pay in: " ++ quote.preferredPayIn) ]
                 ]
-                    ++ (paymentOptionView <| List.head <| List.filter (\p -> p.payIn == quote.preferredPayIn) <| quote.paymentOptions)
+                    ++ (paymentOptionView <| List.head <| List.filter (\p -> p.payIn == quote.preferredPayIn && p.disabled == False) quote.paymentOptions)
+                    ++ noticesView quote.notices
 
         _ ->
             text ""
@@ -74,10 +93,39 @@ paymentOptionView value =
             [ div [] [ text ("Pay out: " ++ option.payOut) ]
             , div [] [ text ("Fee: " ++ String.fromFloat option.feeAmount) ]
             , div [] [ text ("Price: " ++ String.fromFloat option.priceTotalAmount) ]
+            , div [ title option.estimatedDelivery ] [ text ("Estimated delivery: " ++ option.formattedEstimatedDelivery) ]
             ]
 
         _ ->
             [ div [] [ text "No payment options" ] ]
+
+
+noticesView : List Notice -> List (Html msg)
+noticesView notices =
+    case notices of
+        [] ->
+            []
+
+        _ ->
+            List.map noticeView notices
+
+
+noticeView : Notice -> Html msg
+noticeView notice =
+    div [] [ text ("Notice: " ++ noticeTypeView notice.type_ ++ " " ++ notice.text ++ " " ++ notice.link) ]
+
+
+noticeTypeView : NoticeType -> String
+noticeTypeView noticeType =
+    case noticeType of
+        Info ->
+            "ℹ️"
+
+        Warning ->
+            "⚠️"
+
+        Blocked ->
+            "🛑"
 
 
 
@@ -126,7 +174,7 @@ maybeOrNull encoder value =
 
 quoteDecoder : D.Decoder Quote
 quoteDecoder =
-    D.map7 Quote
+    D.map8 Quote
         (D.field "id" D.string)
         (D.field "sourceCurrency" D.string)
         (D.field "targetCurrency" D.string)
@@ -134,12 +182,44 @@ quoteDecoder =
         (D.maybe (D.field "targetAmount" D.float))
         (D.field "preferredPayIn" D.string)
         (D.field "paymentOptions" (D.list paymentOptionDecoder))
+        (D.field "notices" (D.list noticeDecoder))
 
 
 paymentOptionDecoder : D.Decoder PaymentOption
 paymentOptionDecoder =
-    D.map4 PaymentOption
+    D.map7 PaymentOption
+        (D.field "disabled" D.bool)
+        (D.field "estimatedDelivery" D.string)
+        (D.field "formattedEstimatedDelivery" D.string)
         (D.field "payIn" D.string)
         (D.field "payOut" D.string)
         (D.at [ "fee", "total" ] D.float)
         (D.at [ "price", "total", "value", "amount" ] D.float)
+
+
+noticeDecoder : D.Decoder Notice
+noticeDecoder =
+    D.map3 Notice
+        (D.field "text" D.string)
+        (D.field "link" D.string)
+        (D.field "type" noticeTypeDecoder)
+
+
+noticeTypeDecoder : D.Decoder NoticeType
+noticeTypeDecoder =
+    D.string
+        |> D.andThen
+            (\str ->
+                case str of
+                    "INFO" ->
+                        D.succeed Info
+
+                    "WARNING" ->
+                        D.succeed Warning
+
+                    "BLOCKED" ->
+                        D.succeed Blocked
+
+                    _ ->
+                        D.fail <| "Unknown notice type: " ++ str
+            )
